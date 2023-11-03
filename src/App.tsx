1ec6c8f3e6eaf24ed38cd83ad9003a1bcc69a037
Adam Primarizki <adam.primarizki@gmail.com>
2023-11-04 01:54:31 +0700

    initiate project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="max-w-7xl mx-auto px-4">
+      <h1 className="text-red-900 text-3xl font-bold ">Get Yo Stuff Done</h1>
+    </section>
+  );
+}
+
+export default App;
