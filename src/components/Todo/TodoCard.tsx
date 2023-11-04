02db3a40123d32a8d674b39eaf2c94a4a68a29e2
Adam Primarizki <adam.primarizki@gmail.com>
2023-11-05 03:55:57 +0700

    add todo edit on TodoCard

--- src/components/Todo/TodoCard.tsx
+++ src/components/Todo/TodoCard.tsx
@@ -1,21 +1,47 @@
-import { useTodo } from "@/lib/hooks";
+import { useKeyPress, useOutsideClick, useTodo } from "@/lib/hooks";
 import { Todo } from "@/lib/type";
 import { CheckCheckIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 import DeleteTodo from "./DeleteTodo";
 
 type Props = {
   todo: Todo;
 };
 const TodoCard: React.FC<Props> = ({ todo: { title, desc, id } }) => {
-  const { doneTodo } = useTodo();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [editing, setEditing] = useState(false);
+  const [titleState, setTitleState] = useState(title);
+  const [descState, setDescState] = useState(desc || "");
+  const { doneTodo, editTodo } = useTodo();
 
   const handleDoneTodo = () => {
     doneTodo({ id });
   };
 
+  const handleEditTodo = (e?: React.FormEvent<HTMLFormElement>) => {
+    e?.preventDefault();
+    setEditing(false);
+    editTodo({
+      id,
+      newTitle: titleState,
+      newDesc: descState,
+    });
+  };
+
+  const outsideRef = useOutsideClick(handleEditTodo);
+  useKeyPress("Escape", () => {
+    if (editing) setEditing(false);
+  });
+
+  useEffect(() => {
+    if (editing) inputRef.current?.focus();
+  }, [editing, inputRef]);
+
   return (
-    <div className="flex flex-row gap-2">
+    <div className="flex flex-row gap-4">
       <div className="flex-shrink-0 flex gap-2 justify-end sm:justify-start">
         <Button
           size={"icon"}
@@ -27,13 +53,47 @@ const TodoCard: React.FC<Props> = ({ todo: { title, desc, id } }) => {
         </Button>
       </div>
 
-      <div className="bg-card rounded-lg border shadow-sm p-2 px-4 flex gap-2 justify-between">
-        <div className="text-gray-200 flex flex-col gap-2">
-          <p className="text-md font-bold antialiased text-md break-all">
-            {title}
-          </p>
-          {desc && <p className="text-gray-400 text-sm break-all">{desc}</p>}
-        </div>
+      <div className="bg-card rounded-lg border shadow-sm p-2 px-4 flex gap-4 justify-between">
+        {!editing ? (
+          <div
+            className="text-gray-200 flex flex-col gap-2 cursor-pointer"
+            onClick={() => setEditing(true)}
+          >
+            <p className="text-md font-bold antialiased text-md break-all ">
+              {title}
+            </p>
+            {desc && <p className="text-gray-400 text-sm break-all">{desc}</p>}
+          </div>
+        ) : (
+          <div ref={outsideRef}>
+            <form
+              onSubmit={handleEditTodo}
+              className="text-gray-200 flex flex-col gap-2 cursor-pointer"
+            >
+              <Input
+                ref={inputRef}
+                id={`edit-title-${id}`}
+                aria-label="edit-todo-title"
+                required={true}
+                minLength={1}
+                onChange={(e) => setTitleState(e.target.value)}
+                value={titleState}
+                placeholder="Edit New Title"
+                className="text-md font-bold antialiased border-0 rounded-none border-b focus-visible:ring-0 focus-visible:border-b-ring focus-visible:border-b-2 bg-card px-0"
+              />
+              <Input
+                id={`edit-desc-${id}`}
+                aria-label="edit-todo-description"
+                onChange={(e) => setDescState(e.target.value)}
+                value={descState}
+                placeholder="Description"
+                className="text-gray-400 text-sm border-0 rounded-none border-b focus-visible:ring-0 focus-visible:border-b-ring focus-visible:border-b-2 bg-card px-0"
+              />
+              <Button size={"icon"} className="hidden" type="submit"></Button>
+            </form>
+          </div>
+        )}
+
         <DeleteTodo id={id} />
       </div>
     </div>
