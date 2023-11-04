31df1c0652c862ea9396331b9c77fb2061d197e3
Adam Primarizki <adam.primarizki@gmail.com>
2023-11-05 03:01:52 +0700

    include TodoSection and Toaster to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+import TodoSection from "./components/Todo/TodoSection";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
-    <main className="max-w-7xl mx-auto px-4">
-      <section>
-        <h1 className="text-gray-300 text-5xl font-bold my-24">
-          Get Yo Stuff Done
-        </h1>
+    <main className="max-w-7xl mx-auto px-4 min-h-screen flex flex-col">
+      <section className="mb-40">
+        <Menu />
+        <TodoSection />
       </section>
       <Footer />
+      <Toaster />
     </main>
   );
 }
