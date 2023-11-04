75c47c889b1484ed2b9a03ba4682904b14da08dc
Adam Primarizki <adam.primarizki@gmail.com>
2023-11-05 03:54:05 +0700

    Add some instructions on Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,16 +2,25 @@ import { Button } from "./ui/button";
 
 const Menu = () => {
   return (
-    <div className="flex mt-4 mb-24 gap-2 flex-wrap">
-      <Button size="sm" className="text-xs h-fit px-3 py-1">
-        Import
-      </Button>
-      <Button size="sm" className="text-xs h-fit px-3 py-1">
-        Export
-      </Button>
-      <Button variant="secondary" size="sm" className="text-xs h-fit px-3 py-1">
-        Toggle Category View
-      </Button>
+    <div className="mt-4 mb-24">
+      <div className="flex gap-2 flex-wrap mb-4">
+        <Button size="sm" className="text-xs h-fit px-3 py-1">
+          Import
+        </Button>
+        <Button size="sm" className="text-xs h-fit px-3 py-1">
+          Export
+        </Button>
+        <Button
+          variant="secondary"
+          size="sm"
+          className="text-xs h-fit px-3 py-1"
+        >
+          Toggle Category View
+        </Button>
+      </div>
+      <p>
+        Note: Double click todo/category to edit. Drag todo/category to reorder.
+      </p>
     </div>
   );
 };
