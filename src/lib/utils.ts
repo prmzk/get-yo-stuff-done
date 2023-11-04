943935e4b8dc7f82b266444879ff73c547afb5fe
Adam Primarizki <adam.primarizki@gmail.com>
2023-11-05 02:58:11 +0700

    add local storage utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Todos } from "./type";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const setLocalStorageTodo = (value: Todos) => {
+  const stringifiedValue = JSON.stringify(value);
+  window.localStorage.setItem("todos", stringifiedValue);
+  window.dispatchEvent(new Event("storage"));
+};
+
+export const getLocalStorageTodo = () => {
+  return window.localStorage.getItem("todos") ?? '{"data":[]}';
+};
