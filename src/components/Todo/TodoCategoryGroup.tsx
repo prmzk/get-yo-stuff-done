cf511f1aa79952d6bf4cf3fedae2f5fedbd64d96
Adam Primarizki <adam.primarizki@gmail.com>
2023-11-09 18:30:40 +0700

    remove reorder category element

--- src/components/Todo/TodoCategoryGroup.tsx
+++ src/components/Todo/TodoCategoryGroup.tsx
@@ -1,9 +1,7 @@
 import { Todo, TodoCategory } from "@/lib/type";
 import { Reorder } from "framer-motion";
-import { ArrowDown, ArrowUp } from "lucide-react";
 import { useContext } from "react";
 import { TodoContext } from "../context/TodoContext";
-import { Button } from "../ui/button";
 import AddTodo from "./AddTodo";
 import DeleteCategory from "./DeleteCategory";
 import TodoCard from "./TodoCard";
@@ -16,46 +14,12 @@ type Props = {
   todosLength: number;
 };
 
-const TodoCategoryGroup: React.FC<Props> = ({ todos, index, todosLength }) => {
-  const { moveCategoryAction } = useContext(TodoContext);
-
-  const handleMoveCategoryUp = () => {
-    moveCategoryAction({ id: todos.id, dir: "up" });
-  };
-
-  const handleMoveCategoryDown = () => {
-    moveCategoryAction({ id: todos.id, dir: "down" });
-  };
-
+const TodoCategoryGroup: React.FC<Props> = ({ todos }) => {
   return (
     <div className="w-full flex flex-col gap-4">
       <div className="flex items-center gap-2">
         <TodoCategoryTitle title={todos.title} id={todos.id} />
         <DeleteCategory id={todos.id} />
-        <div className="self-start flex items-center">
-          {index !== 0 && (
-            <Button
-              size={"icon"}
-              variant={"ghost"}
-              aria-label="Up Todo"
-              title="Up Todo"
-              onClick={handleMoveCategoryUp}
-            >
-              <ArrowUp className="opacity-75" size={12} strokeWidth={3} />
-            </Button>
-          )}
-          {index !== todosLength - 1 && (
-            <Button
-              size={"icon"}
-              variant={"ghost"}
-              aria-label="Down Todo"
-              title="Down Todo"
-              onClick={handleMoveCategoryDown}
-            >
-              <ArrowDown className="opacity-75" size={12} strokeWidth={3} />
-            </Button>
-          )}
-        </div>
       </div>
       <TodoGroup todos={todos.todos} categoryId={todos.id} />
       <AddTodo categoryId={todos.id} />
